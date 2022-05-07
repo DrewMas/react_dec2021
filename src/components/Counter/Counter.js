32798fd37377fced483faf51d927bad5748e8149
DrewMas <andrewmasnyi@gmail.com>
2022-05-07 18:10:53 +0300

    Home work 4 - done

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -7,23 +7,36 @@ const init = (initCount) => {
 };
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'inc_1':
-            return {...state, count_1: state.count_1 + 1};
-        case 'dec_1':
-            return {...state, count_1: state.count_1 - 1};
+    let {type, payload} = action;
+    switch (type) {
+        // case 'inc_1':
+        //     return {...state, count_1: state.count_1 + 1};
+        // case 'dec_1':
+        //     return {...state, count_1: state.count_1 - 1};
+        // case 'reset_1':
+        //     return {...state, count_1: 0};
+        // case 'inc_2':
+        //     return {...state, count_2: state.count_2 + 1};
+        // case 'dec_2':
+        //     return {...state, count_2: state.count_2 - 1};
+        // case 'reset_2':
+        //     return {...state, count_2: 0};
+        // case 'inc_3':
+        //     return {...state, count_3: state.count_3 + 1};
+        // case 'dec_3':
+        //     return {...state, count_3: state.count_3 - 1};
+        // case 'reset_3':
+        //     return {...state, count_3: 0};
+        case 'counter_1':
+            return {...state, count_1: state.count_1 + payload};
+        case 'counter_2':
+            return {...state, count_2: state.count_2 + payload};
+        case 'counter_3':
+            return {...state, count_3: state.count_3 + payload};
         case 'reset_1':
             return {...state, count_1: 0};
-        case 'inc_2':
-            return {...state, count_2: state.count_2 + 1};
-        case 'dec_2':
-            return {...state, count_2: state.count_2 - 1};
         case 'reset_2':
             return {...state, count_2: 0};
-        case 'inc_3':
-            return {...state, count_3: state.count_3 + 1};
-        case 'dec_3':
-            return {...state, count_3: state.count_3 - 1};
         case 'reset_3':
             return {...state, count_3: 0};
         default:
@@ -45,8 +58,8 @@ function Counter() {
                     {state.count_1}
                 </div>
                 <div className={'btn-wrapper'}>
-                    <button onClick={() => dispatch({type: 'inc_1'})} className={'btn'}>Increment</button>
-                    <button onClick={() => dispatch({type: 'dec_1'})} className={'btn'}>Decrement</button>
+                    <button onClick={() => dispatch({type: 'counter_1', payload: 1})} className={'btn'}>Increment</button>
+                    <button onClick={() => dispatch({type: 'counter_1', payload: -1})} className={'btn'}>Decrement</button>
                     <button onClick={() => dispatch({type: 'reset_1'})} className={'btn'}>Reset</button>
                 </div>
             </div>
@@ -55,8 +68,8 @@ function Counter() {
                     {state.count_2}
                 </div>
                 <div className={'btn-wrapper'}>
-                    <button onClick={() => dispatch({type: 'inc_2'})} className={'btn'}>Increment</button>
-                    <button onClick={() => dispatch({type: 'dec_2'})} className={'btn'}>Decrement</button>
+                    <button onClick={() => dispatch({type: 'counter_2', payload: 1})} className={'btn'}>Increment</button>
+                    <button onClick={() => dispatch({type: 'counter_2', payload: -1})} className={'btn'}>Decrement</button>
                     <button onClick={() => dispatch({type: 'reset_2'})} className={'btn'}>Reset</button>
                 </div>
             </div>
@@ -65,8 +78,8 @@ function Counter() {
                     {state.count_3}
                 </div>
                 <div className={'btn-wrapper'}>
-                    <button onClick={() => dispatch({type: 'inc_3'})} className={'btn'}>Increment</button>
-                    <button onClick={() => dispatch({type: 'dec_3'})} className={'btn'}>Decrement</button>
+                    <button onClick={() => dispatch({type: 'counter_3', payload: 1})} className={'btn'}>Increment</button>
+                    <button onClick={() => dispatch({type: 'counter_3', payload: -1})} className={'btn'}>Decrement</button>
                     <button onClick={() => dispatch({type: 'reset_3'})} className={'btn'}>Reset</button>
                 </div>
             </div>
